eb4fb2dac8de0e18c39a27cff313217bdacf0d83
DESKTOP-PI5E4V1\chodu <choduck99@gmail.com>
2021-04-14 16:18:24 +0900

    .

---
+++ src/main/java/com/kotra/dmin/CatMappingMngApplication.java
@@ -0,0 +1,13 @@
+package com.kotra.dmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatMappingMngApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatMappingMngApplication.class, args);
+	}
+
+}
